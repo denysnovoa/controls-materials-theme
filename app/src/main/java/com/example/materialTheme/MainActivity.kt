24e0bd0ce7bdbf367e0075df92476c4955af56dd
denysnovoa <dnovoa20@gmail.com>
2021-01-09 14:56:11 +0100

    render chips

--- app/src/main/java/com/example/materialTheme/MainActivity.kt
+++ app/src/main/java/com/example/materialTheme/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.materialTheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.materialTheme.controls.Badge
+import com.example.materialTheme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bindChipGroupExtras()
+    }
+
+    private fun ActivityMainBinding.bindChipGroupExtras() {
+        val extras = listOf(
+            "Extra 1",
+            "Extra 2",
+            "Extra 3",
+            "Extra Salto de línea",
+            "Extra 5",
+            "Extra 6",
+            "Extra 7",
+            "Extra 8"
+        )
+
+        extras.forEach { extra ->
+            Badge.create(
+                this@MainActivity,
+                extra,
+                R.style.Badge
+            ).also {
+               chipGroupExtras.addView(it)
+            }
+        }
     }
 }
